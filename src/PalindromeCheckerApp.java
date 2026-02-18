ede7cf43c506837fe7dfd3e6384f99765c89ba60
ritvikaaaa <ritvikaarora08@gmail.com>
2026-02-18 15:45:20 +0530

    Added uc1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
-}
+
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a word or phrase: ");
+            String input = sc.nextLine();
+
+            // Remove spaces and convert to lowercase for uniform comparison
+            String cleaned = input.replaceAll("\\s+", "").toLowerCase();
+
+            // Reverse the string
+            String reversed = new StringBuilder(cleaned).reverse().toString();
+
+            // Check if palindrome
+            if(cleaned.equals(reversed)) {
+                System.out.println("It is a palindrome.");
+            } else {
+                System.out.println("Not a palindrome.");
+            }
+
+            sc.close();
+        }
+    }
+
+
